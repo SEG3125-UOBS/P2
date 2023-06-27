f4cc8274287d4a2defeff14a8ec6971bb15883d0
BS <bsayy072@uottawa.ca>
2023-06-27 15:15:21 -0400

    made layers to nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
-
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
@@ -12,9 +13,18 @@ function BootNav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/store">Store</Nav.Link>
+          <Nav className="flex-grow-1">
+            <Container>
+              <Row className="justify-content-end"> 
+                <Nav.Link className="w-auto" as={Link} to="/">Home</Nav.Link>
+                <Nav.Link className="w-auto" as={Link} to="/store">Store</Nav.Link>
+              </Row>
+              <Row className="justify-content-end">
+                <Nav.Link className="w-auto" as={Link} to="/">Home</Nav.Link>
+                <Nav.Link className="w-auto" as={Link} to="/store">Store</Nav.Link>
+              </Row>
+            </Container>
+            
           </Nav>
         </Navbar.Collapse>
 
